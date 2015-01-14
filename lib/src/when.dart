fef3f8dda18cc5c49d90c0852037327faa706619
Dan Schultz <schultz.t.dan@gmail.com>
2015-01-14 03:15:28 -0800

    Fix issues with returning the incorrect stream type

--- lib/src/when.dart
+++ lib/src/when.dart
@@ -27,10 +27,13 @@ class When<T> implements StreamTransformer<T, T> {
 
   Stream<T> bind(Stream<T> stream) {
     var broadcastStream = stream.asBroadcastStream();
-    return _toggle
-        .transform(new FlatMapLatest((isToggled) {
-          return isToggled ? broadcastStream : new Stream.fromIterable([]);
-        }))
-        .transform(new TakeUntil(broadcastStream.length));
+    return _bindStream(like: stream, onListen: (EventSink<T> sink) {
+      return _toggle
+          .transform(new FlatMapLatest((isToggled) {
+            return isToggled ? broadcastStream : new Stream.fromIterable([]);
+          }))
+          .transform(new TakeUntil(broadcastStream.length))
+          .listen((value) => sink.add(value));
+    });
   }
 }
--- lib/src/zip.dart
+++ lib/src/zip.dart
@@ -33,18 +33,21 @@ class Zip<A, B, R> implements StreamTransformer<A, R> {
   Stream<R> bind(Stream<A> stream) {
     Queue appendToQueue(Queue queue, element) => queue..add(element);
 
-    stream = stream.asBroadcastStream();
+    var broadcastStream = stream.asBroadcastStream();
     var other = _other.asBroadcastStream();
 
-    var bufferA = stream.transform(new Scan(new Queue<A>(), appendToQueue));
+    var bufferA = broadcastStream.transform(new Scan(new Queue<A>(), appendToQueue));
     var bufferB = other.transform(new Scan(new Queue<B>(), appendToQueue));
 
     var combined = Combine.all([bufferA, bufferB]) as Stream<List<Queue>>;
 
-    return combined
-        .where((queues) => queues.first.isNotEmpty && queues.last.isNotEmpty)
-        .map((queues) => _combiner(queues.first.removeFirst(), queues.last.removeFirst()))
-        .transform(new TakeUntil(stream.length))
-        .transform(new TakeUntil(other.length));
+    return _bindStream(like: stream, onListen: (EventSink<R> sink) {
+      return combined
+          .where((queues) => queues.first.isNotEmpty && queues.last.isNotEmpty)
+          .map((queues) => _combiner(queues.first.removeFirst(), queues.last.removeFirst()))
+          .transform(new TakeUntil(broadcastStream.length))
+          .transform(new TakeUntil(other.length))
+          .listen((value) => sink.add(value));
+    });
   }
 }
