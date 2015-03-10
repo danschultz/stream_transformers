523fea2c6db80f4b37253d0d6871701a2c2bc497
Dan Schultz <schultz.t.dan@gmail.com>
2015-03-10 12:47:20 -0700

    Add concat tests to all_tests

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -2,8 +2,12 @@ library all_tests;
 
 import 'buffer_when_test.dart' as buffer_when;
 import 'combine_test.dart' as combine;
+import 'concat_test.dart' as concat;
+import 'concat_all_test.dart' as concat_all;
 
 void main() {
   buffer_when.main();
   combine.main();
+  concat.main();
+  concat_all.main();
 }
